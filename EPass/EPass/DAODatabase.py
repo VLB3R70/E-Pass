c2b401269abebeb35748d55c72a8baa64e705fda
Alberto <vlb3r70@protonmail.com>
2022-04-20 16:47:23 +0200

    [TUI] Added the DAO methods for delete entries. That methods are now implemented in TUI manager

--- EPass/EPass/DAODatabase.py
+++ EPass/EPass/DAODatabase.py
@@ -13,26 +13,38 @@ class DAO:
 
     def __init__(self):
         self.connection = sqlite3.connect(self.DATABASE_NAME)
-        self.createMasterPassTable()
-        self.createUserDataTable()
+        self.setupDataBase()
 
-    def createMasterPassTable(self):
-        SQLMasterTable = \
-            'CREATE TABLE IF NOT EXISTS MasterPassword(Master_Password TEXT, EMail VARCHAR(50))'
+    def setupDataBase(self):
+        SQLStart = """
+        CREATE TABLE IF NOT EXISTS MasterPassword(Master_Password TEXT, EMail VARCHAR(50));
+        CREATE TABLE IF NOT EXISTS UserData(ID INTEGER primary key AUTOINCREMENT , Site_name VARCHAR(100), Username VARCHAR(50), Password TEXT);
+        """
 
         with self.connection:
-            self.connection.execute(SQLMasterTable)
+            self.connection.executescript(SQLStart)
 
-    def saveMasterPassword(self, masterPassword):
+    def resetDataBase(self):
+        SQLReset = """
+        DROP TABLE IF EXISTS MasterPassword;
+        DROP TABLE IF EXISTS UserData;
+        CREATE TABLE IF NOT EXISTS MasterPassword(Master_Password TEXT, EMail VARCHAR(50));
+        CREATE TABLE IF NOT EXISTS UserData(ID INTEGER primary key AUTOINCREMENT , Site_name VARCHAR(100), Username VARCHAR(50), Password TEXT);
+        """
+
+        with self.connection:
+            self.connection.executescript(SQLReset)
+
+    def saveMasterPassword(self, masterPassword, email):
         encryptedPass = self.encryptor.encrypt(password=masterPassword)
-        insertMasterPassword = 'INSERT INTO MasterPassword(Master_Password)' \
-                               'VALUES(?)'
+        insertMasterPassword = 'INSERT INTO MasterPassword(Master_Password, EMail)' \
+                               'VALUES(?,?)'
 
         with self.connection:
-            self.connection.execute(insertMasterPassword, (encryptedPass,))
+            self.connection.execute(insertMasterPassword, (encryptedPass, email))
 
     def getMasterPassword(self):
-        selectMasterPassword = 'SELECT * FROM MasterPassword'
+        selectMasterPassword = 'SELECT Master_Password FROM MasterPassword'
 
         with self.connection:
             result = self.connection.execute(selectMasterPassword).fetchone()
@@ -48,13 +60,6 @@ class DAO:
 
     ########################################################################################
 
-    def createUserDataTable(self):
-        SQLUserData = \
-            'CREATE TABLE IF NOT EXISTS UserData(ID INTEGER primary key , Site_name VARCHAR(100), Username VARCHAR(50), Password TEXT)'
-
-        with self.connection:
-            self.connection.execute(SQLUserData)
-
     def saveUserData(self, siteName, userName, password):
         encryptedPassword = self.encryptor.encrypt(password=password)
         insertUserData = 'INSERT INTO UserData(Site_name, Username, Password)' \
@@ -63,32 +68,52 @@ class DAO:
         with self.connection:
             self.connection.execute(insertUserData, (siteName, userName, encryptedPassword))
 
-    def getUserPassword(self, siteName):
-        selectUserPassword = 'SELECT Password FROM UserData WHERE Site_name LIKE ?'
+    def getUserData(self):
+        selectUserData = 'SELECT * FROM UserData'
+        with self.connection:
+            userData = self.connection.execute(selectUserData).fetchall()
+        return userData
+
+    def getUserPassword(self, ID):
+        selectUserPassword = 'SELECT Password FROM UserData WHERE ID LIKE ?'
 
         with self.connection:
-            userPassword = self.connection.execute(selectUserPassword, (siteName,)).fetchone()
+            userPassword = self.connection.execute(selectUserPassword, (ID,)).fetchone()
             decryptedPassword = self.decryptor.decrypt(password=userPassword[0])
 
         return decryptedPassword
 
-    def getUserName(self, siteName):
-        selectUserName = 'SELECT Username FROM UserData WHERE Site_name LIKE ?'
+    def getUserName(self, ID):
+        selectUserName = 'SELECT Username FROM UserData WHERE ID LIKE ?'
 
         with self.connection:
-            userName = self.connection.execute(selectUserName, (siteName,)).fetchone()
+            userName = self.connection.execute(selectUserName, (ID,)).fetchone()
 
         return userName[0]
 
-    def updateUsername(self, siteName, newUserName):
-        updateUserName = 'UPDATE UserData SET Username = ? WHERE Site_name = ?'
+    def updateUsername(self, ID, newUserName):
+        updateUserName = 'UPDATE UserData SET Username = ? WHERE ID = ?'
 
         with self.connection:
-            self.connection.execute(updateUserName, (newUserName, siteName))
+            self.connection.execute(updateUserName, (newUserName, ID))
 
-    def updateUserPassword(self, siteName, newPassword):
-        updateUserPassword = 'UPDATE UserData SET Password=? WHERE Site_name=?'
+    def updateUserPassword(self, ID, newPassword):
+        updateUserPassword = 'UPDATE UserData SET Password=? WHERE ID=?'
 
         with self.connection:
             encryptedPassword = self.encryptor.encrypt(newPassword)
-            self.connection.execute(updateUserPassword, (encryptedPassword, siteName))
+            self.connection.execute(updateUserPassword, (encryptedPassword, ID))
+
+    def deleteOnePassword(self, ID):
+        deleteOnePassword = 'DELETE FROM UserData WHERE ID=?'
+
+        with self.connection:
+            self.connection.execute(deleteOnePassword, (ID,))
+
+    def deleteManyPasswords(self, IDList):
+        deleteManyPasswords = 'DELETE FROM UserData WHERE ID=?'
+
+        with self.connection:
+            for i in IDList:
+                self.connection.execute(deleteManyPasswords, (i,))
+
--- EPass/TUI/UIManager.py
+++ EPass/TUI/UIManager.py
@@ -4,10 +4,11 @@ from rich.markdown import Markdown
 from rich.prompt import Prompt
 from rich.table import Table
 
+from EPass.EPass.DAODatabase import DAO
 from .TUIHelp import HELP
 
+dao = DAO()
 
-# from EPass.DAODatabase import DAO
 
 def mainMenu():
     optionMenu = createMainMenu()
@@ -19,11 +20,10 @@ def mainMenu():
         if selection == '0':
             nicePrint(optionMenu)
         elif selection == '1':
-            # getUserData()
             nicePrint(createUserDataMenu())
         elif selection == '2':
             id = Prompt.ask("[cyan]Enter the ID of the password you want to get")
-            # getUserPassword(siteName=siteName)
+            dao.getUserPassword(ID=id)
             # copy the password to the clipboard
             nicePrint("[green]Password succesfully copied to the clipboard")
         elif selection == '3':
@@ -31,18 +31,42 @@ def mainMenu():
             newUserName = Prompt.ask("[cyan]Enter your username of the site given", default="")
             newPassword = Prompt.ask("[cyan]Enter the password of te site given", default="", password=True)
 
-            # saveUserData(siteName=newSiteName, userName=newUserName, password=newPassword)
+            dao.saveUserData(siteName=newSiteName, userName=newUserName, password=newPassword)
             nicePrint("[green]Succesfully added a new entry to the database!")
         elif selection == '4':
             id = Prompt.ask("[cyan]Enter the ID of the password you want to update")
             newPassword = Prompt.ask("[cyan]Enter the new password", default="", password=True)
-            nicePrint("[green]Password succesfully updated")
+
+            dao.updateUserPassword(ID=id, newPassword=newPassword)
+            nicePrint("[green]Password succesfully updated!")
+        elif selection == '5':
+            id = Prompt.ask("[cyan]Enter the ID of the username yo want to update")
+            newUserName = Prompt.ask("[cyan]Enter the new username")
+
+            dao.updateUsername(ID=id,newUserName=newUserName)
+            nicePrint("[green]Username succesfully updated!")
+        elif selection == '6':
+            deleteSelection = Prompt.ask("[orange]Do you want to delete one entry or more",default="",choices=['one','more'])
+
+            if deleteSelection == 'one':
+                id = Prompt.ask("[cyan]Enter the ID of the password")
+                dao.deleteOnePassword(ID=id)
+                nicePrint("[green]Password successfully deleted")
+            elif deleteSelection == 'more':
+                id = Prompt.ask("[cyan]Enter the ID's of the passwords you want to delete.\n"
+                                "[yellow]To enter more than one ID just simply enter the number and leave a blank space")
+
+                IDList = list(id.replace(' ',''))
+                dao.deleteManyPasswords(IDList=IDList)
+                nicePrint("[green]Succesfully deleted all passwords!")
+
         elif selection == 'exit':
             nicePrint("[magenta][bold]Goodbye! 👋")
             break
 
 
 def login():
+    login = True
     logo = """[bold][magenta]________         ________      
 |   ____|        |   __  |     
 |   |__    ___   |  |__| |  __ _      ____   ____
@@ -52,29 +76,34 @@ def login():
     description = "\n\n[bold][blue]Welcome to a fast and smart password manager for the linux terminal lovers.😘"
 
     console = Console()
-    # dao = DAO()
 
     nicePrint(logo + description)
-    election = Prompt.ask(prompt='1. Login with master password. \n2. Create a new user\n3. Remember password', choices=['1', '2', '3'])
-    if election == '1':
-        masterPassword = Prompt.ask(prompt='[magenta]Enter the master password', default="", password=True)
+    while login:
+        election = Prompt.ask(prompt='1. Login with master password. \n2. Create a new user\n3. Remember password',
+                              choices=['1', '2', '3'])
+        if election == '1':
+            masterPassword = Prompt.ask(prompt='[magenta]Enter the master password', default="", password=True)
 
-        if masterPassword == 'Alberto2002':
-            nicePrint("[green]Succesful login!")
-            mainMenu()
-        else:
-            nicePrint("[bold][red]Wrong password![yellow]Nice try[/yellow] 😉")
-    elif election == '2':
-        securityChoice = Prompt.ask(
-            "[bold][yellow]WARNING\n[red]If you have a previous user with a master password all the data will be deleted. Are you sure you want to create a new one?",
-            choices=['y', 'n'])
+            if masterPassword == dao.getMasterPassword():
+                nicePrint("[green]Succesful login!")
+                mainMenu()
+                login = False
+            else:
+                nicePrint("[bold][red]Wrong password![yellow]Nice try[/yellow] 😉")
+        elif election == '2':
+            securityChoice = Prompt.ask(
+                "[bold][yellow]WARNING\n[red]If you have a previous user with a master password all the data will be deleted. Are you sure you want to create a new one?",
+                choices=['y', 'n'])
 
-        if securityChoice == 'y':
-            newMasterPassword = Prompt.ask(prompt="Introduce the new master password", default="", password=True)
-            console.log("[bold][italic][red]New master password created and all previous data deleted.")
-    elif election == '3':
-        # send an email with the password
-        pass
+            if securityChoice == 'y':
+                dao.resetDataBase()
+                newMasterPassword = Prompt.ask(prompt="[cyan]Enter the new master password", default="", password=True)
+                newEmail = Prompt.ask("[cyan]Enter your email", default="")
+                dao.saveMasterPassword(masterPassword=newMasterPassword, email=newEmail)
+                console.log("[bold][italic][red]New master password created and all previous data deleted.")
+        elif election == '3':
+            # send an email with the password
+            pass
 
 
 def TUIHelp():
@@ -84,10 +113,9 @@ def TUIHelp():
 
 
 def createUserDataMenu():
-    menu = Table(show_lines=True)
-    menu.add_column("ID")
-    menu.add_column("Site name")
-    menu.add_column("User name")
+    menu = Table("ID", "Site name", "User name", show_lines=True)
+    for row in dao.getUserData():
+        menu.add_row(str(row[0]), row[1], row[2])
 
     return menu
 
