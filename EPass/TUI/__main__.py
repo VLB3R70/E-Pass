7aefb8f7a421dc24a16a0b14877d2cf4d827d77c
Alberto <vlb3r70@protonmail.com>
2022-04-26 17:29:30 +0200

    Project structure changed once again

---
+++ EPass/TUI/__main__.py
@@ -0,0 +1,10 @@
+from .UIManager import TUIHelp
+from .UIManager import login
+
+
+def main():
+    login()
+
+
+def help():
+    TUIHelp()
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='EPass',
       version='0.1.0',
-      packages=find_packages(include=['EPass', 'EPass.*']),
+      packages=find_packages(include=['EPass', 'EPass.*'], exclude=['GUI']),
       install_requires=[],
       entry_points={
-          'console_scripts': ['epass=EPass.__main__:main',
-                              'epass-help=EPass.__main__:help']
+          'console_scripts': ['epass=EPass.TUI.__main__:main',
+                              'epass-help=EPass.TUI.__main__:help']
       })
