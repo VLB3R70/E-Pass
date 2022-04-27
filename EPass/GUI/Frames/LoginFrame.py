0e0f55a863a193052c8c30dc617091133f776909
Alberto <vlb3r70@protonmail.com>
2022-04-27 22:03:38 +0200

    [GUI] Improvements on LoginFrame

--- EPass/GUI/Frames/LoginFrame.py
+++ EPass/GUI/Frames/LoginFrame.py
@@ -1,5 +1,7 @@
 from tkinter import *
-from tkinter.ttk import *
+
+BACKGROUND_COLOR = "#8E24AA"
+BUTTON_COLOR = "#4CAF50"
 
 
 class LoginFrame(Frame):
@@ -9,14 +11,19 @@ class LoginFrame(Frame):
 
         self.title = master.title("Log in")
         self.size = master.minsize(500, 200)
+        master.config(padx=60, pady=50, bg=BACKGROUND_COLOR)
 
         self.img = PhotoImage(file='img/candado.png')
-        self.epassImage = Label(self,image=self.img)
-        self.epassImage.grid(column=2, row=3, pady=15,columnspan=2)
+        self.epassImage = Label(self, image=self.img, bg=BACKGROUND_COLOR)
+        self.epassImage.grid(column=0, row=0, columnspan=2)
 
-        self.masterPasswordLabel = Label(self, text="Master password:")
-        self.masterPasswordLabel.grid(column=2, row=6, pady=70)
-        self.masterPasswordEntry = Entry(self)
-        self.masterPasswordEntry.grid(column=3, row=6, pady=70)
+        self.masterPasswordLabel = Label(self, text="Master password:", bg=BACKGROUND_COLOR)
+        self.masterPasswordLabel.grid(column=0, row=1)
+        self.masterPasswordEntry = Entry(self, show='*')
+        self.masterPasswordEntry.grid(column=1, row=1)
+        self.masterPasswordEntry.focus()
+
+        self.accessButton = Button(self, text="Log in", bg=BUTTON_COLOR)
+        self.accessButton.grid(column=0, row=2, columnspan=2, rowspan=2)
 
         self.pack()
