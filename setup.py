e4135a2fd4c15a7d472af9337b77c9608ad28c52
Alberto <vlb3r70@protonmail.com>
2022-04-29 21:28:46 +0200

    setup.py changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='EPass',
       version='0.1.0',
-      packages=find_packages(include=['EPass', 'EPass.*'], exclude=['GUI']),
+      packages=find_packages(include=['TUI', 'TUI.*', 'API', 'API.*'], exclude=['GUI', 'REST']),
       install_requires=[],
       entry_points={
           'console_scripts': ['epass=TUI.__main__:main',
