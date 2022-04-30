444cf870b0fe481cc549635c37bf0aa6e2de6933
Alberto <vlb3r70@protonmail.com>
2022-04-30 17:47:14 +0200

    Added a new directory for the android project

---
+++ Android/app/src/main/java/com/vlb3r70/epass/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vlb3r70.epass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
