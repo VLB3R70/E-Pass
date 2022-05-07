8afa63869f464f8dda6a505eb8efe2a07a8ff285
Alberto <vlb3r70@protonmail.com>
2022-05-07 11:16:58 +0200

    [REST] Created the models of the database

--- REST/app.py
+++ REST/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
 
 
 @app.route('/home')
---
+++ REST/models.py
@@ -0,0 +1,10 @@
+from app import db
+from sqlalchemy import VARCHAR, Integer, Text
+
+
+class MasterPassword(db.Model):
+    pass
+
+
+class UserData(db.Model):
+    pass
