7881e6e83137d4814491d67abe9afc39710615ec
Alberto <vlb3r70@protonmail.com>
2022-05-18 13:31:54 +0200

    Read the Docs configuration file updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ release = '0.1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_toolbox.more_autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
@@ -53,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
